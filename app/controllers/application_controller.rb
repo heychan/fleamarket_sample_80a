bbc59c81cf1906a476216793c6924909e5b0f4cf
iidakyouhei <kunekune0803@gmail.com>
2020-10-16 19:22:34 +0900

    新規登録,ログインページ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -13,4 +14,10 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
+
+  def configure_permitted_parameters
+    added_attrs = [ :nickname, :email, :password ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
+
 end
