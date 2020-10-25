d585db107295d26c7430ea3a74c756f337cbf311
heychan <68950957+heychan@users.noreply.github.com>
2020-10-25 13:03:40 +0900

    create_showpage_view
    商品詳細ページ画面を作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :show]
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
   end
