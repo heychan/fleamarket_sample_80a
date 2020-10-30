5e26e6993aeee2f0149d7e102e936e6560db68b2
iidakyouhei <kunekune0803@gmail.com>
2020-10-30 18:51:45 +0900

    ユーザページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
+  get 'users/show' => 'users#show'
+  get 'users/out' => 'users#index'
+  get '/mypage' => 'users#mypage'
   devise_for :users
   root 'items#index'
+  resources :users, only: [:show,:destroy]
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
+  end
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
