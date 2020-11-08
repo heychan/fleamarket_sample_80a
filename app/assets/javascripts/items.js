2e6d7e911769df68573fdfc3bc80bf0a495e9149
ma-ri0328 <68724951+ma-ri0328@users.noreply.github.com>
2020-11-08 16:47:02 +0900

    image-upload3

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,20 @@
+$(document).on('turbolinks:load', function(){
+  $(function(){
+    function buildHTML(count) {
+      let html = `<div class="preview-box" id="preview-box__${count}">
+                    <div class="upper-box">
+                      <img src="" alt="preview">
+                    </div>
+                    <div class="lower-box">
+                      <div class="update-box">
+                        <label class="edit_btn">編集</label>
+                      </div>
+                      <div class="delete-box" id="delete_btn_${count}">
+                        <span>削除</span>
+                      </div>
+                    </div>
+                  </div>`
+      return html;
+    }
+  })
+})
